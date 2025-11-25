700ceab975d02478f286971b566c32ddc17face2
DuranUnverdi <duranunverdi1905@gmail.com>
2025-11-25 18:36:50 +0300

    @Entity , @Repository and JPA with create student tablo

---
+++ src/main/java/com/spring_rest_api/entities/Student.java
@@ -0,0 +1,28 @@
+package com.spring_rest_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(name = "birth_date",nullable = true)
+    private Date birthDate;
+
+
+}
--- src/main/java/com/spring_rest_api/springcore/SpringCoreApplication.java
+++ src/main/java/com/spring_rest_api/springcore/SpringCoreApplication.java
@@ -2,11 +2,15 @@ package com.spring_rest_api.springcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = {"com.spring_rest_api"})
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EntityScan(basePackages = "com.spring_rest_api.entities")       // ✔ Entities burada
+@EnableJpaRepositories(basePackages = "com.spring_rest_api.repository") // ✔ Repository burada
+@ComponentScan(basePackages = "com.spring_rest_api")
 public class SpringCoreApplication {
     public static void main(String[] args) {
     SpringApplication.run(SpringCoreApplication.class, args);
